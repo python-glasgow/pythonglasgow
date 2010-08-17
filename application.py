e841cfedab4aa0b9ca0a86043ad5f436a44773ae
Dougal Matthews <dougal85@gmail.com>
2010-08-17 23:44:10 +0100

    Added tweets to the home page

--- application.py
+++ application.py
@@ -1,11 +1,35 @@
-from flask import Flask
-app = Flask(__name__)
+import urllib
 
+from django.utils import simplejson
+from google.appengine.api import memcache
+
+from flask import Flask
 from flask import render_template
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html', );
+
+@app.context_processor
+def get_tweets():
+    
+    tweets = memcache.get("tweets")
+    
+    if tweets is None:
+        
+        url = "https://twitter.com/statuses/user_timeline.json?screen_name=pythonedinburgh"
+        f = urllib.urlopen(url)
+        content = f.read()
+        json = simplejson.loads(content)
+        
+        tweets = {'tweets':json,}
+        
+        if not memcache.add("tweets", tweets, 60 * 10): # 10 mins.
+            logging.error("Memcache set failed.")
+        
+    return tweets
 
 app.secret_key = '7%@0g6y!hu^flbmkcfb$@zxs9ftmh=t0blgnog-ibh52za$6nu'
 
