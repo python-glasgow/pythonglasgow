d800d2beb93b6f14f7ef6388708ea3ad41db9a2f
Dougal Matthews <dougal85@gmail.com>
2010-10-18 20:00:39 +0100

    added error handling to the twitter code and some pep8 fixes

--- application.py
+++ application.py
@@ -1,16 +1,15 @@
 import logging
 import re
-import urllib
 
 from django.utils.html import urlize
 from google.appengine.api import memcache
 from google.appengine.ext import db
 
 from flask import Flask
-from flask import render_template, redirect, request
+from flask import render_template
+import gcal
 from jinja2 import Markup
 import tweepy
-import gcal
 
 app = Flask(__name__)
 
@@ -32,34 +31,39 @@ def home():
 def twitterfy(tweet):
     # find hashtags
     pattern = re.compile(r"(?P<start>.?)#(?P<hashtag>[A-Za-z0-9_]+)(?P<end>.?)")
-    
+
     # replace with link to search
     link = r'\g<start>#<a href="http://search.twitter.com/search?q=\g<hashtag>"  title="#\g<hashtag> search Twitter">\g<hashtag></a>\g<end>'
     text = pattern.sub(link,tweet)
-    
+
     # find usernames
     pattern = re.compile(r"(?P<start>.?)@(?P<user>[A-Za-z0-9_]+)(?P<end>.?)")
-    
+
     # replace with link to profile
     link = r'\g<start>@<a href="http://twitter.com/\g<user>"  title="#\g<user> on Twitter">\g<user></a>\g<end>'
     text = pattern.sub(link,text)
-    
+
     return Markup(urlize(text))
 
 
 def get_tweets():
     tweets = memcache.get("tweets")
-    
+
     if tweets is None:
         credentials = TwitterCredentials.all().get()
         if credentials:
             auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret)
             auth.set_access_token(credentials.access_token, credentials.access_secret)
             api = tweepy.API(auth_handler=auth)
-            statuses = api.user_timeline('pythonedinburgh', count=5)
-        
+
+            # Added for offline development.
+            try:
+                statuses = api.user_timeline('pythonedinburgh', count=5)
+            except tweepy.TweepError:
+                return {}
+
             tweets = [twitterfy(status.text) for status in statuses]
-        
+
             if not memcache.add("tweets", tweets, 60 * 10): # 10 mins.
                 logging.error("Memcache tweet store failed.")
         else:
@@ -67,7 +71,7 @@ def get_tweets():
             credentials = TwitterCredentials(consumer_key='a', access_token='a',
                     consumer_secret='a', access_secret='a')
             db.put(credentials)
-    
+
     return tweets
 
 app.secret_key = '7%@0g6y!hu^flbmkcfb$@zxs9ftmh=t0blgnog-ibh52za$6nu'
