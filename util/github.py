33c91f0697c9a1a34550a21ef9508e67f977826e
Dougal Matthews <dougal85@gmail.com>
2013-04-06 12:01:37 +0100

    Catch errors

--- util/github.py
+++ util/github.py
@@ -1,9 +1,12 @@
-from requests import get
+from requests import get, ConnectionError
 
 
 def get_members():
 
-    members = get("https://api.github.com/orgs/python-glasgow/members").json()
+    try:
+        members = get("https://api.github.com/orgs/python-glasgow/members").json()
+    except ConnectionError:
+        raise StopIteration
 
     for member in members:
         yield get(member['url']).json()
